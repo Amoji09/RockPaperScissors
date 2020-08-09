e22db62d2a9e168b9c0c2c4ce9ea20beaf6a8162
Amogh Mantri <amogh.mantri@yahoo.com>
2020-08-09 16:31:19 -0500

    Initial Commit

---
+++ RPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPS
+//
+//  Created by Amogh Mantri on 8/9/20.
+//  Copyright © 2020 Amogh Mantri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
